94b0c669d2346905dfa9d1cb0c15b452e783b825
mharoot <michaelharootoonyan@yahoo.com>
2019-08-13 22:15:43 -0700

    Create BrokenLinksChecker.js

---
+++ BrokenLinksChecker.js
@@ -0,0 +1,59 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const request = require('request-promise');
+_VISITED = [];
+
+(async function start() {
+  let homepageURL = 'http://yourhomepage.com';
+  _VISITED[homepageURL] = true;
+  await getPageLinks(homepageURL);
+  console.log(_VISITED)
+  for (let pageUrl in _VISITED) {
+    if (! _VISITED[pageUrl]) await getPageLinks(pageUrl);
+  }
+})();
+
+
+
+
+function isValidAnchor(text, href) {
+    return text.length > 0 && href && !href.includes('#') && !href.includes('wp-content');
+}
+
+async function getPageLinks(url) {
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get(url);
+    // await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    // await driver.wait(until.titleIs('webdriver - Google Search'), 5000);
+    let links = await driver.findElements({css:'a'});
+
+  
+    for(let link of links) {
+        
+        var text = await link.getText(),
+        href = await link.getAttribute("href");
+
+        if ( isValidAnchor(text, href) && _VISITED[href] == null) {          
+          _VISITED[href] = false;
+          try {
+           await request({
+                url: href,
+                method: 'GET',
+                data: {
+                }
+              }, function(err, res, body) {
+                console.log({text: text, href: href, statusCode: res.statusCode});
+              });
+            } catch (err) {
+              // We dont want to go to a broken page link to scan for links so we mark true
+              _VISITED[href] = true;
+            }  
+        } 
+            
+    }
+    
+  } finally {
+    await driver.quit();
+  }
+}
+
